e4e287f7bd34e6b02611ab15397a330faee9fa08
Carlos Ignacio Roa <60223426+CIgnacio-dev@users.noreply.github.com>
2025-08-26 02:27:12 -0400

    First commit
    Base project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductDashboardComponent } from './components/product-dashboard/product-dashboard.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ProductDashboardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'product-dashboard';
